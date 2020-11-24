2cc9701ae10e53b19bb3ef08e5d4c3c72f603d69
fkilld <fkill.d.azadd@gmail.com>
2020-11-24 09:36:22 +0530

    Todo Item Component

--- src/App.js
+++ src/App.js
@@ -24,15 +24,12 @@ class App extends Component {
     }
     const updatedItems = [...this.state.items, newItem]
 
-    this.setState(
-      {
-        items: updatedItems,
-        item: '',
-        id: uuid(),
-        editItem: false,
-      },
-      () => console.log(this.state)
-    )
+    this.setState({
+      items: updatedItems,
+      item: '',
+      id: uuid(),
+      editItem: false,
+    })
   }
   clearList = () => {
     console.log('clear list ')
